7dba9340c59e3821a97117e7b4fb07972e04712d
Tam Le <philosocode@gmail.com>
2019-02-23 23:14:13 -0700

    App skeleton mostly done. Most of functionality implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,139 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Bookie</h1>
+    <div class="input-container">
+      <input type="text" v-model="searchTerm">
+      <span v-if="searchTerm.length > 0" @click="searchTerm = ''" class="clear">X</span>
+    </div>
+    <button @click="onSubmit">Search</button>
+    <p v-show="errorMessage" class="error">{{ errorMessage }}</p>
+    <br>
+    <ul v-if="books.length > 0">
+      <Book
+        v-for="book in books"
+        :key="book.id"
+        :title="book.title"
+        :by="book.by"
+        :publisher="book.publisher"
+        :imageUrl="book.imageUrl"
+        :bookUrl="book.bookUrl"
+      />
+    </ul>
+    <p v-else>{{ statusMessage }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+import Book from "./components/Book";
 
 export default {
-  name: 'app',
+  name: "App",
+  data() {
+    return {
+      searchTerm: "",
+      books: [],
+      paginationIndex: 10,
+      totalNumberOfBooks: 0,
+      statusMessage: "",
+      errorMessage: ""
+    };
+  },
+  computed: {
+    query: function() {
+      return this.searchTerm.split(" ").join("+");
+    }
+  },
   components: {
-    HelloWorld
+    Book
+  },
+  methods: {
+    resetQuery() {
+      this.books = [];
+      this.statusMessage = "";
+      this.errorMessage = "";
+      this.paginationIndex = 10;
+    },
+    onSubmit() {
+      this.resetQuery();
+
+      if (!this.searchTerm || !this.searchTerm.replace(/\s/g, "").length) {
+        this.errorMessage = "Enter something please...";
+        return;
+      }
+
+      axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${this.query}`)
+        .then(res => {
+          if (!res.data.items) {
+            this.statusMessage = "No books found";
+            return;
+          }
+
+          const booksData = res.data.items;
+          const books = booksData.map(bookData => {
+            const id = bookData.id;
+            const volume = bookData.volumeInfo;
+
+            const title = volume.title;
+            const publisher = volume.publisher || "Unknown Publisher";
+            const bookUrl = volume.infoLink;
+
+            let imageUrl;
+            if (volume.imageLinks && volume.imageLinks.thumbnail) {
+              imageUrl = volume.imageLinks.thumbnail;
+            } else {
+              imageUrl = "https://b.kisscc0.com/20180817/zzq/kisscc0-empty-book-intentionally-blank-page-book-cover-col-blank-book-5b76dcc0d16560.5728512315345164168577.png";
+            }
+
+            let by;
+            if (!volume.authors) {
+              by = "Unknown";
+            } else if (volume.authors.length > 1) {
+              by = volume.authors[0];
+            } else {
+              by = volume.authors.join(", ");
+            }
+
+            return { id, title, by, publisher, imageUrl, bookUrl };
+          });
+
+          this.totalNumberOfBooks = res.data.totalItems;
+          this.books = books;
+        })
+        .catch(err => console.log(err));
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container {
+  margin: 0 auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 80%;
+}
+
+.input-container {
+  display: inline-block;
+  position: relative;
+  // overflow: hidden;
+}
+
+.clear {
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: .5rem;
+  transition: all .2s ease-in;
+}
+
+.status {
+  color: green;
+}
+
+.error {
+  color: red;
 }
 </style>
---
+++ src/components/Book.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="book">
+    <h2>{{ title }}</h2>
+    <p>By: {{ by }}</p>
+    <p>Published By: {{ publisher }}</p>
+    <img :src="imageUrl" :alt="title">
+    <a class="book__link" :href="bookUrl" target="_blank" rel="noopener">
+      <button>See This Book</button>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Book',
+  props: ["title", "by", "publisher", "imageUrl", "bookUrl"],
+}
+</script>
+
+<style lang="scss" scoped>
+.book {
+  background: $color-grey-light-2;
+  margin: 0 auto;
+  width: 25rem;
+
+  &__link {
+    display: block;
+  }
+}
+</style>
